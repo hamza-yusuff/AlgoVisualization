52f304ce6e55b5bd0ab7b5ba0199a99a803957ff
A S M Taseen <taseen951753@gmail.com>
2021-04-03 01:59:22 +0600

    updated script.js

--- src/script.js
+++ src/script.js
@@ -4,7 +4,8 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
 
 
-const cards = document.querySelectorAll(".card");
+const cards_left = document.querySelectorAll(".card-left");
+const cards_right = document.querySelectorAll(".card-right");
 const titles = document.querySelectorAll(".card-title");
 const texts = document.querySelectorAll(".card-text");
 const buttons = document.querySelectorAll(".btn");
@@ -215,10 +216,55 @@ navSlide();
 // 3D animation of cards
 console.log(window.innerHeight);
 
-cards.forEach(card => {
+cards_left.forEach(card => {
     card.addEventListener("mousemove", (e) => {
         let xAxis = (window.innerWidth / 2 - e.pageX) / 30;
-        card.style.transform = `rotateY(${-xAxis}deg) rotateX(${-xAxis - 10}deg)`;
+        card.style.transform = `rotateY(${xAxis}deg) rotateX(${-xAxis - 10}deg)`;
+    });
+    card.addEventListener("mouseenter", (e) => {
+        card.style.transition = "all 0.2s ease";
+        titles.forEach(title => {
+            title.style.transform = "translateZ(150px)";
+        });
+        texts.forEach(text => {
+            text.style.transform = "translateZ(150px)";
+        });
+        buttons.forEach(button => {
+            button.style.transform = "translateZ(150px)";
+        });
+        images.forEach(image => {
+            image.style.transform = "translateZ(150px)";
+        });
+    });
+    card.addEventListener("mouseleave", (e) => {
+        card.style.transition = "all 1.5s ease";
+        card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+        const titles = document.querySelectorAll(".card-title");
+
+
+        titles.forEach(title => {
+            title.style.transform = "translateZ(0px)";
+        });
+        const texts = document.querySelectorAll(".card-text");
+        texts.forEach(text => {
+            text.style.transform = "translateZ(0px)";
+        });
+        const buttons = document.querySelectorAll(".btn");
+        buttons.forEach(button => {
+            button.style.transform = "translateZ(0px)";
+        });
+        const images = document.querySelectorAll(".icon");
+        imagess.forEach(image => {
+            image.style.transform = "translateZ(0px)";
+        });
+    });
+});
+
+// Animation of left cards
+cards_right.forEach(card => {
+    card.addEventListener("mousemove", (e) => {
+        let xAxis = (window.innerWidth / 2 - e.pageX) / 30;
+        card.style.transform = `rotateY(${-xAxis}deg) rotateX(${xAxis - 10}deg)`;
     });
     card.addEventListener("mouseenter", (e) => {
         card.style.transition = "all 0.2s ease";
